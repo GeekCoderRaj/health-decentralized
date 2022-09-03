2d922a0c87d5a75ef1fcd464d9c7aef5ada9ac92
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-03 16:22:00 +0530

    initialized Moralisprovider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider>
+      <App />
+    </MoralisProvider>
   </React.StrictMode>
 );
