4e38d1f4fbba70bddde5af7146e64a04c07d308f
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-04 00:38:11 +0530

    - homepage finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,48 +5,52 @@ const Home = () => {
     return (
         <div className='home-container'>
             <div className='home'>
-                <div className='navbar-container'>
-                    <div className='navbar'>
-                        <div className='navbar-logo'>
-                            Healthier
-                        </div>
-                        <div className='navbar-connect-btn'>
-                            Connect
-                        </div>
-                    </div>
-                </div>
-                <div className='info-container'>
-                    <div className='info'>
-                        <div className='title-light'>
-                            With
-                            <span className='logo-'>
+                <div className='home-content'>
+                    <div className='navbar-container'>
+                        <div className='navbar'>
+                            <div className='navbar-logo'>
                                 Healthier
-                            </span>
-                        </div>
-                        <div className='title'>
-                            Store Your Medical Records At A Safe Place
-                        </div>
-                        <div className='title-sub'>
-                            A decentralized medical records storage platform that allows you to store your medical records and share them with your doctors and hospitals through QR code.
-                        </div>
-                        <div className='connect-btn'>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">&lt;!--!  Atomicons Free 1.00 by @atisalab License - https://atomicons.com/license/ (Icons: CC BY 4.0) Copyright 2021 Atomicons --&gt;<polyline points="11 17 16 12 11 7"></polyline></svg>
-                            <div className='connect-btn-text'>
+                            </div>
+                            <div className='navbar-connect-btn'>
                                 Connect
                             </div>
                         </div>
-                        <div className='title-patient'>
-                            <div className='title-patient-text'>
-                                Are you a patient?
+                    </div>
+                    <div className='info-container'>
+                        <div className='info'>
+                            <div className='title-light'>
+                                With &nbsp;
+                                <span className='title-logo'>
+                                    Healthier
+                                </span>
                             </div>
-                            <div className='title-patient-btn'>
-                                <Link to='/patient-signup'>
-                                    Join here
-                                </Link>
+                            <div className='title'>
+                                Store Your Medical Records at a Safe Place
+                            </div>
+                            <div className='title-sub'>
+                                A decentralized medical records storage platform that allows you to store your medical records safely and share them with your doctors and hospitals through QR code whenever required.
+                            </div>
+                            <div className='connect-btn'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="#005ac2" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">&lt;!--!  Atomicons Free 1.00 by @atisalab License - https://atomicons.com/license/ (Icons: CC BY 4.0) Copyright 2021 Atomicons --&gt;<polyline points="11 17 16 12 11 7"></polyline></svg>
+                                <div className='connect-btn-text'>
+                                    Connect
+                                </div>
+                            </div>
+                            <div className='title-patient'>
+                                <div className='title-patient-text'>
+                                    Are you a patient?
+                                </div>
+                                <div className='title-patient-btn'>
+                                    <Link to='/patient-signup'>
+                                        Join here
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className='other-content'>
                 <div className='resources-container'>
                     <div className='resources'>
                         <div className='resources-title'>
@@ -80,7 +84,7 @@ const Home = () => {
                             <div className='cards'>
                                 <div className='card-container'>
                                     <div className='card'>
-                                        <div className='card-image'></div>
+                                        <div className='card-image1'></div>
                                         <div className='card-title'>
                                             Admins add the doctor's profile after verifying them
                                         </div>
@@ -91,7 +95,7 @@ const Home = () => {
                                 </div>
                                 <div className='card-container'>
                                     <div className='card'>
-                                        <div className='card-image'></div>
+                                        <div className='card-image2'></div>
                                         <div className='card-title'>
                                             Verified doctors create patient profiles
                                         </div>
@@ -102,7 +106,7 @@ const Home = () => {
                                 </div>
                                 <div className='card-container'>
                                     <div className='card'>
-                                        <div className='card-image'></div>
+                                        <div className='card-image3'></div>
                                         <div className='card-title'>
                                             Patients can view thier reports
                                         </div>
@@ -113,6 +117,18 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className='footer-container'>
+                <div className='footer'>
+                    <div className='copy-text'>
+                        &copy; {new Date().getFullYear()} Healthier Co. | Significant Bits
+                    </div>
+                    <div className='github-btn'>
+                        <a href='https://github.com/GeekCoderRaj/health-decentralized' target='_blank' rel='noreferrer'>
+                            Github
+                        </a>
                     </div>
                 </div>
             </div>
