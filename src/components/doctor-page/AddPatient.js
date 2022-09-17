c21db66ba498c14169357b327b9419658e82705b
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-17 18:41:48 +0530

    created add and view pages

--- src/components/doctor-page/AddPatient.js
+++ src/components/doctor-page/AddPatient.js
@@ -40,6 +40,49 @@ const AddPatient = () => {
                     </div>
                 </div>
                 <div className='content-container'>
+                    <div className='content'>
+                        <div className='form-container'>
+                            <div className='form'>
+                                <div className='form-fields'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='First Name' />
+                                    </div>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Last Name' />
+                                    </div>
+                                </div>
+                                <div className='form-fields'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Gender' />
+                                    </div>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Age' />
+                                    </div>
+                                </div>
+                                <div className='form-fields'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Mobile No.' />
+                                    </div>
+                                    <div className='form-field'>
+                                        <input type='email' placeholder='Email id' />
+                                    </div>
+                                </div>
+                                <div className='form-fields-add'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Address' />
+                                    </div>
+                                </div>
+                                <div className='form-fields-btn'>
+                                    <div className='form-field-btn'>
+                                        Add
+                                    </div>
+                                    <div className='form-field-btn'>
+                                        Cancel
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/patient-page/BookAppointment.js
+++ src/components/patient-page/BookAppointment.js
@@ -34,6 +34,180 @@ const BookAppointment = () => {
                     </div>
                 </div>
                 <div className='content-container'>
+                    <div className='content-view'>
+                        <div className='content-nav-container'>
+                            <div className='content-nav'>
+                                <div className='content-nav-item'>
+                                    Doctors
+                                </div>
+                                <div className='content-nav-item'>
+                                    <div className='search-box'>
+                                        <input type='text' placeholder='Search' />
+                                        <svg width="24" height="24" fill="#a1a1a1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8Zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6Z"></path>
+                                            <path d="M11.412 8.587c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.14-1.512-5.652 0L8.586 8.59c.76-.758 2.07-.756 2.826-.002Z"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='doc-cards-container'>
+                            <div className='doc-cards'>
+                                <div className='doc-card-container'>
+                                    <div className='doc-card'>
+                                        <div className='doc-card-left'>
+                                        </div>
+                                        <div className='doc-card-right'>
+                                            <div className='doc-right-top'>
+                                                <div className='doc-name'>
+                                                    Dr. Jack Sparrow
+                                                </div>
+                                                <div className='doc-spec'>Pirate</div>
+                                                <div className='doc-hospital'>
+                                                    Carribean Hoist
+                                                </div>
+                                                <div className='doc-address'>
+                                                    7/25 East Coast, SA
+                                                </div>
+                                            </div>
+                                            <div className='doc-right-bottom'>
+                                                <div className='doc-right-btn'>
+                                                    Book Appointment
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='doc-card-container'>
+                                    <div className='doc-card'>
+                                        <div className='doc-card-left'>
+                                        </div>
+                                        <div className='doc-card-right'>
+                                            <div className='doc-right-top'>
+                                                <div className='doc-name'>
+                                                    Dr. Jack Sparrow
+                                                </div>
+                                                <div className='doc-spec'>Pirate</div>
+                                                <div className='doc-hospital'>
+                                                    Carribean Hoist
+                                                </div>
+                                                <div className='doc-address'>
+                                                    7/25 East Coast, SA
+                                                </div>
+                                            </div>
+                                            <div className='doc-right-bottom'>
+                                                <div className='doc-right-btn'>
+                                                    Book Appointment
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='doc-card-container'>
+                                    <div className='doc-card'>
+                                        <div className='doc-card-left'>
+                                        </div>
+                                        <div className='doc-card-right'>
+                                            <div className='doc-right-top'>
+                                                <div className='doc-name'>
+                                                    Dr. Jack Sparrow
+                                                </div>
+                                                <div className='doc-spec'>Pirate</div>
+                                                <div className='doc-hospital'>
+                                                    Carribean Hoist
+                                                </div>
+                                                <div className='doc-address'>
+                                                    7/25 East Coast, SA
+                                                </div>
+                                            </div>
+                                            <div className='doc-right-bottom'>
+                                                <div className='doc-right-btn'>
+                                                    Book Appointment
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='doc-cards'>
+                                <div className='doc-card-container'>
+                                    <div className='doc-card'>
+                                        <div className='doc-card-left'>
+                                        </div>
+                                        <div className='doc-card-right'>
+                                            <div className='doc-right-top'>
+                                                <div className='doc-name'>
+                                                    Dr. Jack Sparrow
+                                                </div>
+                                                <div className='doc-spec'>Pirate</div>
+                                                <div className='doc-hospital'>
+                                                    Carribean Hoist
+                                                </div>
+                                                <div className='doc-address'>
+                                                    7/25 East Coast, SA
+                                                </div>
+                                            </div>
+                                            <div className='doc-right-bottom'>
+                                                <div className='doc-right-btn'>
+                                                    Book Appointment
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='doc-card-container'>
+                                    <div className='doc-card'>
+                                        <div className='doc-card-left'>
+                                        </div>
+                                        <div className='doc-card-right'>
+                                            <div className='doc-right-top'>
+                                                <div className='doc-name'>
+                                                    Dr. Jack Sparrow
+                                                </div>
+                                                <div className='doc-spec'>Pirate</div>
+                                                <div className='doc-hospital'>
+                                                    Carribean Hoist
+                                                </div>
+                                                <div className='doc-address'>
+                                                    7/25 East Coast, SA
+                                                </div>
+                                            </div>
+                                            <div className='doc-right-bottom'>
+                                                <div className='doc-right-btn'>
+                                                    Book Appointment
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='doc-card-container'>
+                                    <div className='doc-card'>
+                                        <div className='doc-card-left'>
+                                        </div>
+                                        <div className='doc-card-right'>
+                                            <div className='doc-right-top'>
+                                                <div className='doc-name'>
+                                                    Dr. Jack Sparrow
+                                                </div>
+                                                <div className='doc-spec'>Pirate</div>
+                                                <div className='doc-hospital'>
+                                                    Carribean Hoist
+                                                </div>
+                                                <div className='doc-address'>
+                                                    7/25 East Coast, SA
+                                                </div>
+                                            </div>
+                                            <div className='doc-right-bottom'>
+                                                <div className='doc-right-btn'>
+                                                    Book Appointment
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
