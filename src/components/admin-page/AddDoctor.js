e74b7418836824a492216c57b9e7695186f6b2d9
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-17 13:53:58 +0530

    created add doctor and view doctor

--- src/components/admin-page/AddDoctor.js
+++ src/components/admin-page/AddDoctor.js
@@ -37,6 +37,54 @@ const AddDoctor = () => {
                     </div>
                 </div>
                 <div className='content-container'>
+                    <div className='content'>
+                        <div className='form-container'>
+                            <div className='form'>
+                                <div className='form-fields'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='License No.' />
+                                    </div>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Specialization' />
+                                    </div>
+                                </div>
+                                <div className='form-fields'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='First Name' />
+                                    </div>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Last Name' />
+                                    </div>
+                                </div>
+                                <div className='form-fields'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Mobile No.' />
+                                    </div>
+                                    <div className='form-field'>
+                                        <input type='email' placeholder='Email id' />
+                                    </div>
+                                </div>
+                                <div className='form-fields-add'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Hospital/Clinic' />
+                                    </div>
+                                </div>
+                                <div className='form-fields-add'>
+                                    <div className='form-field'>
+                                        <input type='text' placeholder='Address' />
+                                    </div>
+                                </div>
+                                <div className='form-fields-btn'>
+                                    <div className='form-field-btn'>
+                                        Add
+                                    </div>
+                                    <div className='form-field-btn'>
+                                        Cancel
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
